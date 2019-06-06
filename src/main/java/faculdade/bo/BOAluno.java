7ba1dbfa60f8cfad07253d662e4060b7becd1645
Thales Victor <38298038+thales-victor@users.noreply.github.com>
2019-06-06 12:55:38 -0300

    criando base BOAluno

--- src/main/java/faculdade/bo/BOAluno.java
+++ src/main/java/faculdade/bo/BOAluno.java
@@ -1,4 +1,43 @@
 package faculdade.bo;
 
 public class BOAluno {
+
+    public static List<TOAluno> lista() throws Exception {
+        try (Connection conn = Data.openConnection()) {
+            return DAOAluno.lista(conn);
+        }
+    }
+
+    public static TOAluno inserir(TOAluno model) throws Exception {
+        try (Connection conn = Data.openConnection()) {
+
+            TOAluno aluno = DAOAluno.getByEmail(conn, model);
+            if (aluno != null) {
+                return null;
+            }
+
+            model.setId(Guid.getString());
+            model.setSenha(Encrypt.sha1(model.getSenha()));
+            DAOAluno.inserir(conn, model);
+            
+            return model;
+
+        }
+    }
+
+    public static void atualizar(TOAluno model) throws Exception {
+        try (Connection conn = Data.openConnection()) {
+            DAOAluno.atualizar(conn, model);
+        }
+    }
+
+    public static TOAluno deletar(String id) throws Exception {
+
+        try (Connection conn = Data.openConnection()) {
+
+            TOAluno aluno = DAOAluno.deletar(conn, id);
+
+            return aluno;
+        }
+    }
 }
