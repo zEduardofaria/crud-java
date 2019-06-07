bd0f040f30e84aa7603c35314486a0e7bd615146
Thales Victor <38298038+thales-victor@users.noreply.github.com>
2019-06-07 12:42:59 -0300

    Update BOMateria.java

--- src/main/java/faculdade/bo/BOMateria.java
+++ src/main/java/faculdade/bo/BOMateria.java
@@ -1,8 +1,40 @@
 package faculdade.bo;
 
+import faculdade.dao.DAOMateria;
 import faculdade.fw.Data;
+import faculdade.to.TOMateria;
+
 import java.sql.Connection;
 import java.util.List;
 
 public class BOMateria {
+
+    public static List<TOMateria> lista() throws Exception {
+        try (Connection conn = Data.openConnection()) {
+            return DAOMateria.lista(conn);
+        }
+    }
+
+    public static TOMateria inserir(TOMateria model) throws Exception {
+        try (Connection conn = Data.openConnection()) {
+
+            DAOMateria.inserir(conn, model);
+            
+            return model;
+        }
+    }
+
+    public static void atualizar(TOMateria model) throws Exception {
+        try (Connection conn = Data.openConnection()) {
+            DAOMateria.atualizar(conn, model);
+        }
+    }
+
+    public static void deletar(String id) throws Exception {
+
+        try (Connection conn = Data.openConnection()) {
+
+            DAOMateria.deletar(conn, id);
+        }
+    }
 }
