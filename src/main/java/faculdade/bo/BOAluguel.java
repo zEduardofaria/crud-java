7751af1d815d338f4fb3e29d0866e11f69679f40
Thales Victor <38298038+thales-victor@users.noreply.github.com>
2019-06-07 12:42:28 -0300

    Update BOAluguel.java

--- src/main/java/faculdade/bo/BOAluguel.java
+++ src/main/java/faculdade/bo/BOAluguel.java
@@ -1,8 +1,40 @@
 package faculdade.bo;
 
+import faculdade.dao.DAOAluguel;
 import faculdade.fw.Data;
+import faculdade.to.TOAluguel;
+
 import java.sql.Connection;
 import java.util.List;
 
 public class BOAluguel {
+
+    public static List<TOAluguel> lista() throws Exception {
+        try (Connection conn = Data.openConnection()) {
+            return DAOAluguel.lista(conn);
+        }
+    }
+
+    public static TOAluguel inserir(TOAluguel model) throws Exception {
+        try (Connection conn = Data.openConnection()) {
+
+            DAOAluguel.inserir(conn, model);
+            
+            return model;
+        }
+    }
+
+    public static void atualizar(TOAluguel model) throws Exception {
+        try (Connection conn = Data.openConnection()) {
+            DAOAluguel.atualizar(conn, model);
+        }
+    }
+
+    public static void deletar(String id) throws Exception {
+
+        try (Connection conn = Data.openConnection()) {
+
+            DAOAluguel.deletar(conn, id);
+        }
+    }
 }
